a13f44bc6cb0f2a143a210045e319232cf97ba9d
silvioflorentino <professorsilvioflorentino@yahoo.com.br>
2021-09-27 11:48:37 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Tabuada
+{
+    partial class TelaTabuada
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lBoxResul = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(171, 89);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(100, 23);
+            this.txtNum.TabIndex = 0;
+            this.txtNum.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNum_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Número:";
+            // 
+            // lBoxResul
+            // 
+            this.lBoxResul.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lBoxResul.FormattingEnabled = true;
+            this.lBoxResul.ItemHeight = 25;
+            this.lBoxResul.Location = new System.Drawing.Point(100, 130);
+            this.lBoxResul.Name = "lBoxResul";
+            this.lBoxResul.Size = new System.Drawing.Size(194, 279);
+            this.lBoxResul.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tempus Sans ITC", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(24, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Tabuada";
+            // 
+            // TelaTabuada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lBoxResul);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNum);
+            this.Name = "TelaTabuada";
+            this.ShowIcon = false;
+            this.Text = "Tabuada";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lBoxResul;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Tabuada/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tabuada
+{
+    public partial class TelaTabuada : Form
+    {
+        public TelaTabuada()
+        {
+            InitializeComponent();
+        }
+
+        private void CalcularTabuada()
+        {
+            lBoxResul.Items.Clear();
+           
+            int valor = Convert.ToInt32(txtNum.Text);
+            for( int i = 0; i<=10; i++)
+            {
+                lBoxResul.Items.Add(valor + " x " + i + " = " + (valor * i ));
+            }
+        }
+
+        private void txtNum_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar >= '0' && e.KeyChar <= '9' || e.KeyChar == '\b' || e.KeyChar == '\r')
+            {
+                if(e.KeyChar == '\r')
+                {
+                    CalcularTabuada();
+                }
+                txtNum.Focus();
+                txtNum.Clear();
+            }
+            else
+            {
+                e.KeyChar = '\0';
+            }
+           
+        }
+    }
+}
